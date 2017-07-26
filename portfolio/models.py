4c68d5a139445123a29e787475f7c74422222f17
cvenkatesan <cvenkatesan@unomaha.edu>
2017-07-25 22:42:16 -0500

    Portfolio changes to efs

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from yahoo_finance import Share
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
@@ -58,6 +59,8 @@ class Stock(models.Model):
     shares = models.DecimalField (max_digits=10, decimal_places=1)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_date = models.DateField(default=timezone.now, blank=True, null=True)
+    current_stock_price = models.DecimalField (max_digits=10, decimal_places=2)
+    current_stock_value = models.DecimalField (max_digits=10, decimal_places=2)
 
     def created(self):
         self.recent_date = timezone.now()
@@ -68,3 +71,17 @@ class Stock(models.Model):
 
     def initial_stock_value(self):
         return self.shares * self.purchase_price
+
+    def current_stock_price(self):
+        symbol_f = self.symbol
+        data = Share(symbol_f)
+        share_value = (data.get_open())
+        return share_value
+
+    def current_stock_value(self):
+        symbol_f = self.symbol
+        data = Share(symbol_f)
+        share_value = (data.get_open())
+        if share_value is not None:
+          return float(share_value) * float(self.shares)
+
